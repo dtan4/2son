aa60a270a9bba1ca1a69d45244454d35e4b6a0c1
dtan4 <dtan4@mac>
2013-03-02 23:54:09 +0900

    implement subject_to_hashmap

--- lib/twochannel_util.rb
+++ lib/twochannel_util.rb
@@ -8,9 +8,28 @@ require 'zlib'
 require 'nkf'
 
 module TwoChannelParser
+  def subject_to_hashmap(board_url)
+    subject_url = get_subject_url(board_url)
+    res = fetch_url(subject_url)
+    subject = {"board_url" => board_url}
+
+    case res
+    when Net::HTTPOK
+      subject["result"] = true
+      body = extract_gz(res.body)
+      subject_map = parse_subject_body(body)
+      subject["subject"] = subject_map
+      subject["count"] = subject_map.length
+    else
+      subject["result"] = false
+    end
+
+    return subject
+  end
+
   def thread_to_hashmap(thread_url, skip_aa, skip_nl)
     dat_url, kako_url = get_dat_url(thread_url)
-    res = fetch_dat(dat_url)
+    res = fetch_url(dat_url)
     thread = {"thread_url" => thread_url}
 
     case res
@@ -18,18 +37,18 @@ module TwoChannelParser
       thread["result"] = true
       thread["archived"] = false
       body = extract_gz(res.body)
-      res_map = parse_body(body, skip_aa, skip_nl)
+      res_map = parse_dat_body(body, skip_aa, skip_nl)
       thread["res"] = res_map
       thread["count"] = res_map.length
     when Net::HTTPNonAuthoritativeInformation
-      kako_res = fetch_dat(kako_url)
+      kako_res = fetch_url(kako_url)
 
       case kako_res
       when Net::HTTPSuccess
         thread["result"] = true
         thread["archived"] = true
         body = extract_gz(kako_res.body)
-        res_map = parse_body(body, skip_aa, skip_nl)
+        res_map = parse_dat_body(body, skip_aa, skip_nl)
         thread["res"] = res_map
         thread["count"] = res_map.length
       else
@@ -43,9 +62,24 @@ module TwoChannelParser
   end
 
   private
+  def get_subject_url(board_url)
+    if board_url && validate_board_url(board_url)
+      arr = board_url.split('/')
+      host = arr[2]
+      board = arr[3]
+
+      subject_url =
+        "http://#{host}/#{board}/subject.txt"
+
+      return subject_url
+    else
+      return nil
+    end
+  end
+
   # http://toro.2ch.net/test/read.cgi/famicom/1361411122/
   def get_dat_url(thread_url)
-    if thread_url != nil && validate_thread_url(thread_url)
+    if thread_url && validate_thread_url(thread_url)
       arr = thread_url.split('/')
       host = arr[2]
       board = arr[5]
@@ -62,11 +96,15 @@ module TwoChannelParser
     end
   end
 
+  def validate_board_url(board_url)
+    return (/http:\/\/[0-9a-z.]+\.2ch\.net\/[0-9a-z]+.*/ =~ board_url)
+  end
+
   def validate_thread_url(thread_url)
     return (/http:\/\/[0-9a-z.]+\.2ch\.net\/test\/read\.cgi\/[0-9a-z]+\/\d+.*/ =~ thread_url)
   end
 
-  def fetch_dat(dat_url)
+  def fetch_url(dat_url)
     uri = URI.parse(dat_url)
     req = Net::HTTP::Get.new(uri.path)
     # http://info.2ch.net/wiki/index.php?monazilla%2Fdevelop%2Faccess
@@ -87,7 +125,20 @@ module TwoChannelParser
     return body
   end
 
-  def parse_body(body, skip_aa, skip_nl)
+  def parse_subject_body(body)
+    # Shift-JIS -> UTF-8
+    subject_body = NKF.nkf("-w", body)
+    subject_map = []
+
+    subject_body.each_line do |line|
+      dat, subject, count = parse_subject_line(line)
+      subject_map << {"dat" => dat, "subject" => subject, "count" => count}
+    end
+
+    return subject_map
+  end
+
+  def parse_dat_body(body, skip_aa, skip_nl)
     # Shift-JIS -> UTF-8
     thread_body = NKF.nkf("-w", body)
 
@@ -108,6 +159,20 @@ module TwoChannelParser
     end
 
     return res_map
+  end
+
+  def parse_subject_line(line)
+    line_arr = line.split("<>")
+    dat = line_arr[0]
+    subject = ""
+    count = nil
+
+    if /(.*)\((\d+)\)/ =~ line_arr[1]
+      subject = $1.strip
+      count = $2.to_i
+    end
+
+    return dat, subject, count
   end
 
   def parse_res_line(line, skip_aa, skip_nl)
