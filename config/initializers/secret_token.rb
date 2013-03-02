2f03ea10dbaf1b2e28cc84f57a443fb00e22de13
dtan4 <dtan4@mac>
2013-03-02 16:29:17 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twoson::Application.config.secret_token = '1bb986e01170150b6d235c2543bafd28ab026b0c59ed3189f1e5ffb993cef7fc0686a29099800c520641c380a5d53e50725777e2c39334e8ebfa43317a99b05e'
